4213d9aa7257ca85caa4f19f3bc0b9088e9dc718
Menne Bos <mennebos@gmail.com>
2025-12-12 16:03:52 +0100

    minimal code in class

--- __init__.py
+++ __init__.py
@@ -1,12 +1,26 @@
 import bluetooth
 import threading
+import subprocess
 
 
 from ovos_plugin_manager.phal import PHALPlugin   # due to hivemind fakebus
 from ovos_bus_client.message import Message       # due to hivemind fakebus
 from ovos_utils.log import LOG
 
-CHANNEL = 3
+CHANNEL = 1
+
+# --------------------------------------
+#  get the RSSI
+# --------------------------------------
+def get_rssi(mac):
+    """Vraag RSSI op via hcitool (werkt alleen als device verbonden is)."""
+    try:
+        output = subprocess.check_output(["hcitool", "rssi", mac], text=True)
+        # output bv: "RSSI return value: -45"
+        return int(output.strip().split()[-1])
+    except Exception as e:
+        print("Kon RSSI niet ophalen:", e)
+        return None
 
 class AtomBTPlugin(PHALPlugin):
     def __init__(self, bus=None, config=None):
@@ -29,19 +43,37 @@ class AtomBTPlugin(PHALPlugin):
         server_sock.listen(1)
 
         LOG.info(f"RFCOMM server active on channel {CHANNEL}, waiting for ESP32...")
-        client_sock, client_info = server_sock.accept()
-        LOG.info(f"RFCOMM connected to: {client_info}")
-
         try:
-            while True:
-                data = client_sock.recv(1024)
-                if not data:
-                    break
-                LOG.info(f"Ontvangen: {data.decode()}")
-                client_sock.send(b"Hallo ESP32\n")
-        except OSError:
-            pass
+            while True:    
+                client_sock, client_info = server_sock.accept()
+                LOG.info(f"RFCOMM connected to: {client_info}")
+                
+                mac = client_info[0]
 
-        LOG.info("Verbinding gesloten")
-        client_sock.close()
-        server_sock.close()
+                rssi = get_rssi(mac)
+                LOG.info("Huidige RSSI:", rssi)
+                if rssi is not None:
+                    msg = f"RSSI:{rssi}\n"
+                    client_sock.send(msg)
+                    LOG.info("Verstuurd:", msg)
+
+                try:
+                    while True:
+                        data = client_sock.recv(1024)
+                        if not data:
+                            break
+                        LOG.info(f"Ontvangen: {data.decode()}")
+                        client_sock.send(b"Hallo ESP32\n")
+                except OSError:
+                    pass
+
+        except Exception as e:
+            print("Server error:", e)
+
+        finally:
+            if client_sock:
+                client_sock.close()
+            if server_sock:
+                server_sock.close()
+            LOG.info("Verbinding gesloten")
+
